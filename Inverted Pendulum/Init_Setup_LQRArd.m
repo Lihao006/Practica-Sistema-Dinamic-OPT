21d72c5593915b1d5f0fa005da346ef559d3b4ee
Lihao006 <lihao.jin@estudiantat.upc.edu>
2026-05-16 18:10:11 +0200

    a

--- Inverted Pendulum/Init_Setup_LQRArd.m
+++ Inverted Pendulum/Init_Setup_LQRArd.m
@@ -3,17 +3,7 @@ clc
 r = 0.006;
 M_c = 0.135;
 I = 0.0007176;
-l = 0.2;   % podemos cambiar para que el péndulo es 0.6m
-% Más complicado de controlar o no?
-% En cuanto la 'x' no cambia (lo de moverme me da igual). La primera
-% posición.
-
-% Pero ahora para controlar el péndulo tengo que aplicar más fuerza (para
-% controlar el ángulo). La segunda posición.
-
-% Gasto más si el carro es gordo o la masa de arriba es gordo (acción de
-% control más grande)
-
+l = 0.2;
 g = 9.81;
 b= 0.00007892;
 L = 0.046;
@@ -21,10 +11,7 @@ Rm = 12.5;
 kb = 0.031;
 kt = 0.031;
 c = 0.63;
-m = 0.1;    % la masa que está arriba (m=0.4)
-% La bola que está arriba es 4 veces más pesada
-
-
+m = 0.1;
 M = 0.136;
 Er = 2*m*g*l;
 n= 3;
@@ -41,33 +28,9 @@ A  =  [0 0 1 0; 0 0 0 1; 0 aa -bb -cc; 0 dd -ee -ff];
 B = [0;0; mm; nn]; 
 Q = diag([1200 1500 0 0]);
 R  = 0.035;
-KK = lqr(A,B,Q,R) 
+KK = lqr(A,B,Q,R)
 p1 = [i*2.8; -i*2.8; i*1.5; -i*1.5]; % oscillatory
 p2 =[-8+i*2; -8-i*2; -7+i*2; -7-i*2 ]; % underdamped
 p3 =[-8; -10; -4.5; -5.8];  % stable. 
 p4 =[-20; -15.5; -45.5; -4.8];  % Fast or Aggressive.
 k = place(A,B,p3);
-
-
-
-% Peras-manzanas (posición-ángulo)
-% 100 metros, me puedo equivocar en 100, ahora multiplico 100 por 1000
-% la grua va colgando para llevar peso -> se mueve (muy poco) 20 grados,
-% como trabajo en SI estoy trabajando con radianes -> 0'8. 
-%
-
-% Si fuera 0'8 y 100, en vez de 100 sería 1000 más 2 ceros (desde un punto
-% de vista de 0/1). Tenemos que trabajar en un escalado de 0/1 o -1/1.
-% TENEMOS QUE ESCALAR (TENEMOS QUE ESCALAR BIEN LOS VALORES). DETERMINAR
-% BIEN LA ZONA DE TRABAJO.
-
-% R = 0.035 (valor petit). 1. vull que sigui petit o 2. perquè la zona en
-% la qual treballa l'acció de control no és 0-1. 
-%
-% Corriente continua, 24V coche (estamos trabajando entre 0-24). Este 0,035
-% para escalarlo lo tendríamos que multiplicar por 24 para compararlo con
-% lo del 1000 100??  TENEMOS QUE DEFINIRLO PORQUE ES IMPORTANTE.
-
-% Si no tenemos en cuenta esto es porque NO HEMOS TENIDO EN CUENTA TODO
-% ESTO.
-%
